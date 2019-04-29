c0e70ec740ec169fd5c2d0acfd790384da9f4bc3
Miller David Saavedra <millerdsg@gmail.com>
2019-04-28 21:24:58 -0500

    Convert MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Nancy Suarez on 28/04/19.
+//  Copyright © 2019 Miller David. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
